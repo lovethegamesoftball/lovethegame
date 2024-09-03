0b4f4f36e99679cf087f2064e33f6bf4138be0fb
Tyler Krupicka <tylerkrupicka@gmail.com>
2024-09-02 21:32:20 -0700

    get hero image working

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,33 +4,47 @@ import Navbar from "./Navbar.vue";
 
 <template>
   <div class="hero-container">
-    <section class="hero is-halfheight hero-image">
+    <section class="hero">
       <div class="hero-head"><Navbar /></div>
 
       <div class="hero-body">
         <div class="container has-text-centered">
-          <p class="title has-text-light">Half height hero</p>
-          <p class="subtitle has-text-light">Half height subtitle</p>
+          <h1 class="title has-text-dark has-text-weight-bold is-size-2">
+            Sarah Ogle Softball Coaching
+          </h1>
+          <p class="subtitle has-text-dark">
+            Pitching, hitting, and fielding lessons in San Diego
+          </p>
         </div>
       </div>
     </section>
+    <section class="hero is-medium hero-image"></section>
   </div>
 </template>
 
 <style scoped>
 .hero-container {
   width: 100%;
-  background-color: rgb(203, 204, 215);
 }
+
+.booking-color {
+  color: rgb(15, 139, 141);
+}
+
 .hero-image {
+  height: 40vh;
   margin-left: auto;
   margin-right: auto;
-  max-width: 1248px;
+  max-width: 1000px;
   background-attachment: scroll;
-  background-image: url("../assets/running.webp");
+  background-image: url("../assets/hero-image-medium.png");
   background-repeat: no-repeat;
-  background-position-x: 20%;
   background-size: cover;
-  color: white;
+  @media (min-width: 1000px) {
+    background-image: url("../assets/hero-image-wide.png");
+  }
+  @media (max-width: 680px) {
+    background-image: url("../assets/hero-image-narrow.png");
+  }
 }
 </style>
