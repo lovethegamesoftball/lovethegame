af2aebf69f2637a98b23319ff5ea417f233a6f9e
lovethegamesoftball <sarahoglesoftballcoaching@gmail.com>
2024-09-22 22:55:45 -0700

    added summary text

--- src/components/About.vue
+++ src/components/About.vue
@@ -11,27 +11,34 @@
       </div>
     </section>
     <section class="meet">
-      <h2
-        class="title has-text-dark has-text-weight-bold is-size-3 has-text-centered-mobile"
-      >
+      <h2 class="title has-text-dark has-text-weight-bold is-size-3 has-text-centered-mobile">
         Meet Coach Sarah
       </h2>
       <div class="columns">
         <div class="column is-one-third">
-          <img
-            class="portrait"
-            src="../assets/portrait.jpg"
-            alt="A photo of coach sarah standing in front of a view of la jolla"
-          />
+          <img class="portrait" src="../assets/portrait.jpg"
+            alt="A photo of coach sarah standing in front of a view of la jolla" />
         </div>
-        <div class="column">
-          I provide private and small group softball pitching, hitting, and
-          defense lessons to athletes ages 7-18. My pitching coach, Bill
-          Corbett, instilled this motto in me: "Always love the game". Thus, I
-          seek to foster a love for softball in my clients while they have fun
-          and improve each day. My college softball pitching and over five years
-          of softball coaching experience have prepared me to provide high-level
-          softball instruction.
+        <div class="column text-about">
+
+          <p>I have over five years of softball coaching experience in San Diego working with athletes ages 7-17. My
+            pitching coach, Bill Corbett, instilled this motto in me: "Always love the game". I always seek to foster a
+            love for softball in my clients and make sure they have fun while improving each day.</p>
+          <br>
+
+          <p>My favorite memories growing up are from playing Peninsula Breakers All-stars for Coach Dad, especially
+            qualifying for our first State tournament. I played travel softball for Power Surge and Renegades, including
+            winning the 16U Colorado Sparkler Tournament.</p>
+          <br>
+
+          <p>At Carleton College, I pitched and played 2B while earning a Geology degree in 2019. As one of the team
+            captains my senior year, I helped lead our team to break the Carleton single-season win record. I am now a
+            Ph.D. Candidate in Climate Science at UCSD. It has been a joy to coach in my hometown and work with the next
+            generation of young
+            athletes!</p>
+
+
+
         </div>
       </div>
     </section>
@@ -43,11 +50,16 @@
   padding: 0px 24px 24px 24px;
 }
 
+.text-about {
+  font-size: 1.1rem;
+}
+
 .meet {
   padding: 24px;
   border-radius: 12px;
   border: 3px double rgb(15, 139, 141);
 }
+
 .portrait {
   @media (max-width: 780px) {
     display: block;
